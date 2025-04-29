1f1ed2958bc509c481b9397e67241817f76eb1a4
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-04-29 19:25:53 -0300

    Agregue toda la estructura principal del proyecto

---
+++ js/ejercicio1.js
@@ -0,0 +1,4 @@
+/*1- Crea un objeto llamado auto que tenga algunas características como el color, 
+marca, modelo y si está encendido o apagado. Crea los métodos necesarios para permitir 
+encender y apagar el auto.
+*/
