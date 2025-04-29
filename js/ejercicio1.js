433cade42ecbcffcd8c8ec765f31420133732016
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-04-29 19:43:10 -0300

    Resolvi ejercicio 1

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -2,3 +2,25 @@
 marca, modelo y si está encendido o apagado. Crea los métodos necesarios para permitir 
 encender y apagar el auto.
 */
+
+const auto={
+    //caracteristicas
+    color:"azul",
+    marca:"peugeut",
+    modelo: "208",
+    //metodos
+    encender: function(){
+        console.log(this)
+        document.writeln(`<p>Se encendió el auto</p>`)
+    },
+    apagar: function(){
+        console.log(this)
+        document.writeln(`<p>Se apagó el auto</p>`)
+    }
+}
+
+console.log(auto)
+document.writeln(`<p>El auto ${auto.marca} ${auto.modelo} es de color ${auto.color}</p>`)
+
+auto.encender()
+auto.apagar()
